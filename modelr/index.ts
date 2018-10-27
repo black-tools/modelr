361fc89f71c491591bb3b4ba01c138ec78f5a31e
danfergo <danfergo@gmail.com>
2018-10-27 19:34:32 +0100

    Removed stores from index.ts; added them to tsconfig.json

--- modelr/index.ts
+++ modelr/index.ts
@@ -5,7 +5,4 @@ export * from './decorators/remote'
 export * from './interfaces/store'
 export * from './interfaces/pool'
 
-export * from './stores/rest-store'
-export * from './stores/sequelize-store'
-
 export type RemoteResult<Args, Result> = (args?: Args) => Promise<Result>;
