620b162de9e3b94d5a04e6e71f5688bdc21acec3
danfergo <danfergo@gmail.com>
2018-12-02 20:34:17 +0000

    Fix 2

--- modelr/collection.ts
+++ modelr/collection.ts
@@ -1,10 +1,16 @@
 export function Collection<T>(classType) {
-    return class extends Array<T> {
+    const CollectionClass = class extends Array<T> {
         public static readonly __is_collection__ = true;
         public static readonly __class__ = classType;
+
+        constructor(items?: Array<T>) {
+            super(...items);
+            Object.setPrototypeOf(this, Object.create(CollectionClass.prototype));
+        }
 
         saveAll() {
             // let resources = this.db.save(this)
         }
     }
+    return CollectionClass;
 }
