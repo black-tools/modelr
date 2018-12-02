a4825ba2f1df969c15431d68402ff00f00035e1a
danfergo <danfergo@gmail.com>
2018-12-02 20:28:14 +0000

    trying fug fix for: A parameter property is only allowed in a constructor implementation

--- modelr/collection.ts
+++ modelr/collection.ts
@@ -3,10 +3,6 @@ export function Collection<T>(classType) {
         public static readonly __is_collection__ = true;
         public static readonly __class__ = classType;
 
-        constructor(public store, ...args: any[]) {
-            super(...args);
-        }
-
         saveAll() {
             // let resources = this.db.save(this)
         }
